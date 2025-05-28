aa14fcdf9a1b23bce20d3f061b16d822218c1b48
Yared <yaredseib@gmail.com>
2025-05-28 12:28:36 +0200

    Updated calculation for fahrenheit

--- sensor_data.py
+++ sensor_data.py
@@ -4,11 +4,10 @@ from datetime import datetime, UTC
 class SensorData:
     def __init__(self,
                  temperature_celsius: float,
-                 temperature_fahrenheit: float,
                  humidity: float,
                  pressure: float):
         self.timestamp = datetime.now(UTC)
         self.temperature_celsius = temperature_celsius
-        self.temperature_fahrenheit = temperature_fahrenheit
+        self.temperature_fahrenheit = temperature_celsius * 1.8 + 32
         self.humidity = humidity
         self.pressure = pressure
