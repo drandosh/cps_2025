45ae5576267a62cf18bba3b3a2df33799b538b4e
Yared <yaredseib@gmail.com>
2025-05-28 13:47:56 +0200

    refactoring

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
-import models
+from models import SensorData
+from data_storage_service import DataStorageService
 
-DataStorageService = DataStorageService()
+def main():
+    try:
+        run()
+    except:
+        print('An unknown error occurred, shutting down')
+        input('Press any key to exit')
 
-def print_sensor_data(data: models.SensorData) -> None:
+def run() -> None:
+    data_storage_service = DataStorageService()
+
+
+def print_sensor_data(data: SensorData) -> None:
     print(f'============={data.timestamp}=============')
-    print(f'Temperatur (Celsius) {data.temperature_celsius}')
-    print(f'Temperatur (Fahrenheit) {data.temperature_fahrenheit}')
+    print(f'Temperatur (Celsius): {data.temperature_celsius}')
+    print(f'Temperatur (Fahrenheit): {data.temperature_fahrenheit}')
     print(f'Luftdruck: {data.pressure}')
     print(f'Luftfeuchtigkeit: {data.humidity}')
+    print()
 mock_data = SensorData(27790.0000, 25216.7968700, 100.92763)
 DataStorageService.store(mock_data)
 
--- models.py
+++ models.py
@@ -3,12 +3,12 @@ from datetime import datetime, UTC
 
 class SensorData:
     def __init__(self,
-                 raw_temperature_celsius: float,
+                 temperature_celsius: float,
                  humidity: float,
                  pressure: float):
         self.timestamp = datetime.now(UTC)
-        self.temperature_celsius = (raw_temperature_celsius / 1000)
-        self.temperature_fahrenheit = raw_temperature_celsius * 1.8 + 32
+        self.temperature_celsius = temperature_celsius
+        self.temperature_fahrenheit = temperature_celsius * 1.8 + 32
         self.humidity = humidity
         self.pressure = pressure
 
